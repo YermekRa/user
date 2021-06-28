4fcaa40da2a11baf942f94080bd88bcc7cd3e54b
TEMIRLAN <temirlanturikanov@gmail.com>
2021-06-28 09:57:28 +0600

    add user-service to gateway

---
+++ src/main/java/kz/bcc/balatime/userservice/controller/TestController.java
@@ -0,0 +1,15 @@
+package kz.bcc.balatime.userservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class TestController {
+
+    @GetMapping("/test")
+    public String getString (){
+        return "Hi!";
+    }
+}
